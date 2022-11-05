ec57c533668b751c9b49f75d7be16e040e544493
Duulat Akmatbekov <duulat.akmatbekov@gmail.com>
2022-11-05 11:55:03 +0600

    Deleted all unnecessary files.
    Created assets folder with images of menu block.
    
    Created [order,setOrder] useState array.
    
    Created two fieldsets of future main blocks.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './App.css'
 
 function App() {
+
+  const [order, setOrder] = useState([
+    {name:'Hamburger', price:110, count:0},
+    {name:'Cheeseburger', price:140, count:0},
+    {name:'Shawerma', price:160, count:0},
+    {name:'Fries', price:50, count:0},
+    {name:'Cola', price:40, count:0},
+    {name:'Tea', price:20, count:0},
+    {name:'Coffee', price:80, count:0},
+    {name:'Salad', price:70, count:0},
+  ]);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <fieldset className="order-block">
+        <legend>Order</legend>
+      </fieldset>
+      <fieldset className="items-block">
+        <legend>Menu</legend>
+      </fieldset>
     </div>
   );
 }
